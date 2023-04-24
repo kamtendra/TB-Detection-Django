002dae7d41bdf166d3af892f6f3f1a41653d455c
kamtendra <skamtendra@gmail.com>
2023-04-24 22:31:36 +0530

    Added more models

--- TB_Detection/mainapp/utils.py
+++ TB_Detection/mainapp/utils.py
@@ -9,6 +9,10 @@ model_path = r'C:\Users\kamte\OneDrive\Documents\Desktop\TB Detection\TB_Detecti
 model = load_model(model_path)
 # model.summary()
 
+model2 = load_model("C:\\Users\\kamte\\Downloads\\TB.h5")
+model3 = load_model("C:\\Users\\kamte\\Downloads\\tuberculosis.h5")
+model4 = load_model("C:\\Users\\kamte\\Downloads\\my_model2.hdf5")
+
 def load_img(image_path):
   test_data = []
   image=image_path
@@ -26,6 +30,78 @@ def load_img(image_path):
 def predict_label(image_path):
   result="Normal"
   image = load_img(image_path)
+  predict_model = model.predict(np.array(image))
+  if np.argmax(predict_model) == 1:
+    result="Tuberculosis"
+    return result
+  else:
+    return result
+
+def load_img1(image_path):
+  test_data = []
+  image=image_path
+  img = cv2.imread(str(image))
+  img = cv2.resize(img, (512,512))
+  if img.shape[2] ==1:
+    img = np.dstack([img, img, img])
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img=np.array(img)
+  img = img/255
+  test_data.append(img)
+  test_data1 = np.array(test_data)
+  return test_data1
+
+def predict_label1(image_path):
+  result="Normal"
+  image = load_img1(image_path)
+  predict_model = model2.predict(np.array(image))
+  if int(predict_model.round()) == 1:
+    result="Tuberculosis"
+    return result
+  else:
+    return result
+  
+def load_img2(image_path):
+  test_data = []
+  image=image_path
+  img = cv2.imread(str(image))
+  img = cv2.resize(img, (300,300))
+  if img.shape[2] ==1:
+    img = np.dstack([img, img, img])
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img=np.array(img)
+  img = img/255
+  test_data.append(img)
+  test_data1 = np.array(test_data)
+  return test_data1  
+
+def predict_label2(image_path):
+  result="Normal"
+  image = load_img2(image_path)
+  predict_model = model3.predict(np.array(image))
+  if int(predict_model.round()) == 1:
+    result="Tuberculosis"
+    return result
+  else:
+    return result
+  
+def load_img3(image_path):
+  test_data = []
+  image=image_path
+  img = cv2.imread(str(image))
+  img = cv2.resize(img, (28,28))
+  if img.shape[2] ==1:
+    img = np.dstack([img, img, img])
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img=np.array(img)
+  img = img/255
+  test_data.append(img)
+  test_data1 = np.array(test_data)
+  return test_data1
+
+def predict_label3(image_path):
+  result="Normal"
+  image = load_img3(image_path)
   predict_model = model.predict(np.array(image))
   if np.argmax(predict_model) == 1:
     result="Tuberculosis"
--- TB_Detection/mainapp/views.py
+++ TB_Detection/mainapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.files.storage import default_storage
-from .utils import predict_label
+from .utils import predict_label, predict_label1, predict_label2, predict_label3
 
 def home(request):
     if request.method == 'POST':
@@ -9,8 +9,16 @@ def home(request):
         path= r"C:\Users\kamte\OneDrive\Documents\Desktop\TB Detection\TB_Detection\public\static"
         path+=r"/"
         path+=str(image)
-        result=predict_label(path)
-        print(result)
+        ans=[]
+        ans.append(predict_label(path))
+        ans.append(predict_label1(path))
+        ans.append(predict_label2(path))
+        ans.append(predict_label3(path))
+        print(ans)
+        if ans.count('Tuberculosis'):
+            result = 'Tuberculosis'
+        else:
+            result = 'Normal'    
         return render(request,'index.html', {"result":result})
     return render(request,'index.html') 
 
